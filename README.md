3c2dc4597929df7a3c1ac180c04336f552413cf6
1dayluo <mar1sadaze@qq.com>
2021-06-06 00:46:45 +0800

    v1.0.0

--- README.md
+++ README.md
@@ -27,24 +27,67 @@
 >       -   对于压缩包格式的邮件，会先解压
 >       -   普通附件，直接计算md5
 
+输出：
 
+-   全部邮件信息（`.json`)
 
-参考TEMP：
+-   所有带有附件的邮件信息(`.json`)
 
-````python
+-   md5(`.json`) <u>待优化</u>
 
-TEMP_DICT = {
-    'file_name': '',  # 离线文件名
-    'mail_size': '',  # 邮件大小
-    'subject': '',  # 邮件主题
-    'sender': '',  # 发送人
-    'receiver': '',  # 收件人
-    'cc': '',  # 抄送
-    'attachment': '',  # 附件
-    'attach_size': '',  # 附件大小
-    'attach_md5': ''  # 附件md5值
+    
+
+输出示例：
+
+````json
+
+{
+    "attach": [
+        {
+            "attach_name": "test.tar",
+            "attach_md5": "59dea35cdf6ae0d1faf8ecfa85fd1b55",
+            "attach_size": "0.236MB",
+            "addition": [
+                {
+                    "filename": "13qzx1.png",
+                    "md5": "3ed83a02e9de4f0f90aabcc4188e81e4",
+                    "size": "0.234MB"
+                }
+            ]
+        },
+        {
+            "attach_name": "test(1).tar",
+            "attach_md5": "8af090d03ae366c5e761d9ec9e9d9d7a",
+            "attach_size": "0.106MB",
+            "addition": [
+                {
+                    "filename": "9f124a2fb89871dde11bbd7b5743ccfd06e81b42.jpg",
+                    "md5": "36ebe2522b8343d341a7e1200fe4d7cf",
+                    "size": "0.105MB"
+                }
+            ]
+        },
+        {
+            "attach_name": "test(2).tar",
+            "attach_md5": "e422de5ab0afe00439e9aa7878f1521d",
+            "attach_size": "0.45MB",
+            "addition": [
+                {
+                    "filename": "7bd9ec968738adcc41608ba680c6bafd461ab41172c79-2uao3R.png",
+                    "md5": "f56b15c04531e66f7e15a23689f425c9",
+                    "size": "0.448MB"
+                }
+            ]
+        }
+    ],
+    "subject": "attach_files",
+    "receiver": "*@qq.com",
+    "sender": "*@foxmail.com",
+    "cc": "*@qq.com",
+    "date": "2021-6-5 22:49:32",
+    "eml_size": "1.089MB",
+    "file_name": "attach_test2.eml"
 }
-
 ````
 
 
@@ -55,11 +98,26 @@ TEMP_DICT = {
 
 -   [x] eml读取，提取基础信息（收件人，发件人，cc）
 
--   [ ] eml附件信息
+-   [x] eml附件信息(差一个rar的)
 
--   [ ] 爬虫
+-   [x] 解决附件名称一样的情况
+
+-   [ ] 邮件内url
+
+-   [ ] 爬虫查找md5 有无历史记录
 
     
 
 ## 运行说明
+
+````bash
+usage: run.py [-h] [-mail MAIL] [-md5 MD5] [-md5default]
+
+optional arguments:
+  -h, --help   show this help message and exit
+  -mail MAIL   input path of dir,and read all format of eml files
+  -md5 MD5     choice target dir
+  -md5default  default，read input file
+
+````
 
--- run.py
+++ run.py
@@ -4,27 +4,20 @@
 import os
 import hashlib
 import datetime
+import json
 from argparse import ArgumentParser
-import undetected_chromedriver.v2 as uc
+
 from pprint import pprint
+from collections import Counter
 import mimetypes
+import gzip,tarfile,zipfile,py7zr
+import undetected_chromedriver.v2 as uc
 from email import message_from_file
-from email.header import decode_header
+from email.header import decode_header,Header
 from email.utils import parseaddr,parsedate
-TEMP_DICT = {
-    'file_name': '',  # 离线文件名
-    'eml_size': '',  # 邮件大小
-    'subject': '',  # 邮件主题
-    'sender': '',  # 发送人
-    'receiver': '',  # 收件人
-    'date': '',     #发件日期
-    'cc': '',  # 抄送
-    'attachment_name': '',  # 附件
-    'attach_size': '',  # 附件大小
-    'attach_md5': ''  # 附件md5值
-}
 
 
+MAIL_OUTPUT_DIR = 'Attachment'
 def current_file(path='input',file_lists=[]):
     """
     遍历文件-包括子文件夹下的文件
@@ -50,6 +43,7 @@ def calc_md5(files):
     md5dict = {}
 
     for file in files:
+        print(os.getcwd())
         with open(file, 'rb') as fp:
             data = fp.read()
 
@@ -58,8 +52,95 @@ def calc_md5(files):
     return md5dict
 
 
+def _dir(path):
+    if os.path.isdir(path):
+        pass
+    else:
+        os.mkdir(path)
+    return path
 
-def read_mail(eml_path='mail/helloworld2.eml'):
+def get_FileSize(filePath):
+    """
+    获取文件大小 单位：MB
+    :param filePath:
+    :return:
+    """
+    fsize = os.path.getsize(filePath)
+    fsize = fsize / float(1024 * 1024)
+    return round(fsize, 3)
+
+
+def save_attach(attach_name,data,mail_info):
+    attachment_info = {}
+    check_type = attach_name.split('.')[-1]
+    save_path = MAIL_OUTPUT_DIR + '/' + '[{}]'.format(mail_info['file_name']) + attach_name
+
+    if not os.path.exists(MAIL_OUTPUT_DIR):
+        os.mkdir(MAIL_OUTPUT_DIR)
+
+    with open(save_path,'wb') as f:
+        f.write(data)
+
+    attachment_info['attach_name'] = attach_name
+    attachment_info['attach_md5'] = hashlib.md5(data).hexdigest()
+    attachment_info['attach_size'] = "{}MB".format(get_FileSize(save_path))
+    attachment_info['addition'] = []
+    add_dict = {}
+
+    if check_type in ['zip','tar','gz','7z']: #压缩包类型
+
+        # archive_dir = _dir(MAIL_OUTPUT_DIR + '/' + 'Archive')
+
+
+
+        extarct_dir = _dir(MAIL_OUTPUT_DIR + '/' + attach_name.split('.')[:-1][0])
+
+
+        if check_type == 'zip':
+            zip_file = zipfile.ZipFile(save_path)
+            with zip_file:
+                for names in zip_file.namelist():
+                    names_path = extarct_dir + '/' + names
+                    zip_file.extract(names, extarct_dir)
+                    add_dict['filename'] = names
+                    add_dict['md5'] = calc_md5([names_path]).get(names_path)
+                    add_dict['size'] = '{}MB'.format(get_FileSize(names_path))
+                    attachment_info['addition'].append(add_dict)
+
+
+        if check_type == 'rar':
+            pass
+        if check_type == 'tar':
+            tar_file = tarfile.open(save_path)
+
+            with tar_file:
+                for names in tar_file.getnames():
+                    names_path = extarct_dir + '/' + names
+                    tar_file.extract(names, extarct_dir)
+                    add_dict['filename'] = names
+                    add_dict['md5'] = calc_md5([names_path]).get(names_path)
+                    add_dict['size'] = '{}MB'.format(get_FileSize(names_path))
+                    attachment_info['addition'].append(add_dict)
+
+        if check_type == '7z':
+
+            sevenz_file = py7zr.SevenZipFile(save_path)
+            with sevenz_file:
+                os.chdir(extarct_dir)
+                for names in sevenz_file.getnames():
+                    names_path = extarct_dir + '/' + names
+                    # os.mkdir(names)
+                    sevenz_file.extract('.',names)
+                    add_dict['filename'] = names
+                    add_dict['md5'] = calc_md5([names]).get(names)
+                    add_dict['size'] = '{}MB'.format(get_FileSize(names))
+                    attachment_info['addition'].append(add_dict)
+            os.chdir('../..')
+
+
+    return attachment_info
+
+def read_mail(eml_path='mail/extarct_file.eml'):
     """
     输入：邮件地址
     输出：json（邮件的信息）
@@ -67,6 +148,11 @@ def read_mail(eml_path='mail/helloworld2.eml'):
     :return:
     """
     def msg_date(msg_date):
+        """
+        mail日期
+        :param msg_date:
+        :return:
+        """
         date_list = [str(i) for i in parsedate(msg_date)][:-3]
         year = date_list[:1]
 
@@ -76,13 +162,12 @@ def read_mail(eml_path='mail/helloworld2.eml'):
         return date_str
 
 
-    def get_FileSize(filePath):
-        fsize = os.path.getsize(filePath)
-        fsize = fsize / float(1024 * 1024)
-        return round(fsize, 3)
 
 
-    mail_info = TEMP_DICT
+
+    mail_info = {}
+    mail_info['attach'] = []
+
     if os.path.exists(eml_path):
         with open(eml_path) as f:
             msg = message_from_file(f)
@@ -93,31 +178,67 @@ def read_mail(eml_path='mail/helloworld2.eml'):
             mail_info['cc'] = parseaddr(msg.get('Cc'))[1]
             mail_info['date'] =  msg_date(msg['Date'])
             mail_info['eml_size'] = "{}MB".format(get_FileSize(eml_path))
-            pprint(mail_info)
+            mail_info['file_name'] = eml_path.split('/')[-1]
+            attach_name_list = []
             for part in msg.walk():
                 if part.get_content_maintype() == 'multipart':continue
                 filename = part.get_filename() #获取附件
                 # print(msg.keys())
 
-
-
                 if filename:
-                    print(filename)
+                    savename = filename
+
+                    if filename in attach_name_list:
+                        # print(Counter(attach_name_list)[filename])
+
+                        savename = ''.join(filename.split('.')[:-1]) + '(' + str(Counter(attach_name_list)[filename]) + ').' + ''.join(filename.split('.')[-1:])
 
 
+                    data = part.get_payload(decode=True)
+                    attachment_info  = save_attach(savename, data, mail_info)
+
+                    attach_name_list.append(filename)
+                    mail_info['attach'].append(attachment_info)
+
+            pprint(mail_info)
+
+        return mail_info
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('-target',nargs=1,help=u'选择目标目录')
-    parser.add_argument('-default',action="store_true",help=u'默认模式，读取当前目录下input文件夹')
+    parser.add_argument('-mail',nargs=1,help=u'input path of dir,and read all format of eml files')
+    parser.add_argument('-md5',nargs=1,help=u'choice target dir')
+    parser.add_argument('-md5default',action="store_true",help=u'default，read input file')
     args = parser.parse_args()
 
 
-    if args.target:
+    if args.md5:
         all_files = current_file(args.target[0])
 
-    if args.default:
+    if args.md5default:
         all_files = current_file()
         print(calc_md5(all_files))
+    if args.mail:
+        files = current_file(args.mail[0])
+        mails = [] # all
+        mails_attach = [] # has attach
+        md5list = []
+        for file in files:
+            mail_info = read_mail(file)
+            mails.append(mail_info)
+            if mail_info.get('attach') :
+                if len(mail_info.get('attach')) > 0:
+                    mails_attach.append(mail_info)
 
-    read_mail()
+                    md5list.extend([ {attach['attach_name'] :attach['attach_md5']} for attach in mail_info['attach']])
+
+
+        with open('all_mail.json','w',encoding='utf-8') as f:
+            json.dump({'data': mails}, f, ensure_ascii=False, indent=2)
+
+        with open('attach_mail.json','w',encoding='utf-8') as f:
+            json.dump({'data': mails_attach, 'md5list': md5list}, f, ensure_ascii=False,indent=2)
+
+
+        with open('md5list.json','w',encoding='utf-8') as f:
+            json.dump({'md5list': md5list}, f, ensure_ascii=False, indent=2)
